ba60bfbaf1860067ce568336db5b78986edde322
Dylan Subramanian <77794302+dylsub@users.noreply.github.com>
2022-06-23 17:43:26 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Hero></Hero>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hero.js
@@ -0,0 +1,31 @@
+import "./Hero.css";
+import Logo from "./static/logo.jpg";
+
+function Hero() {
+    return <div>
+        <div className="hero">
+            <div className="nav">
+                <div className="nav__brand">
+                    <img className="nav__logo" src={Logo}></img>
+                </div>
+                <div className="nav__links">
+                    <h2 className="nav__link">Home</h2>
+                    <h2 className="nav__link">About Us</h2>
+                    <h2 className="nav__link">Our Programs</h2>
+                    <h2 className="nav__link">Our Team</h2>
+                </div>
+            </div>
+            <div className="header">
+                <h1 className="header__title">STEM For-Youth, By-Youth</h1>
+                <div className="header__links">
+                    <a className="header__link">Learn More</a>
+                    <a className="header__link">Schedule</a>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+}
+
+export default Hero;
