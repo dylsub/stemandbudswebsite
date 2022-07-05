c0f94b2154d2eec5def4aec004cd1a94fd8907c1
dylsub <dylansubramanian@gmail.com>
2022-07-05 13:14:59 -0500

    Added tape decor that scales with screen widdth

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -9,12 +9,13 @@ import six from "../static/6.png";
 import seven from "../static/7.png";
 import eight from "../static/8.png";
 import paperbordertop from "../static/paperbordertop.png";
+import tape from "../static/tape.png";
 
 function Gallery() {
     return <div className="gallery">
         <img className="border__top" src={paperbordertop}></img>
         <div className="gallery__row">
-            <div><img src={one}></img></div>
+            <div> <img src={one}></img></div>
             <div><img src={two}></img></div>
             <div><img src={three}></img></div>
             <div><img src={four}></img></div>
@@ -26,6 +27,12 @@ function Gallery() {
             <div><img src={eight}></img></div>
         </div>
         <img className="border__bottom" src={paperbordertop}></img>
+        <img className="tape1 tape tape__top" src={tape}></img>
+        <img className="tape2 tape tape__top" src={tape}></img>
+        <img className="tape3 tape tape__top" src={tape}></img>
+        <img className="tape4 tape tape__bottom" src={tape}></img>
+        <img className="tape5 tape tape__bottom" src={tape}></img>
+        <img className="tape6 tape tape__bottom" src={tape}></img>
     </div>
 }
 
